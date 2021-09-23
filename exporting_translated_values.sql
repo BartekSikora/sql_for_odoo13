2942380873eb519272263debfbb520baec5a1bbb
Bartek Sikora <58654131+BartekSikora@users.noreply.github.com>
2021-09-23 14:12:13 +0200

    Create exporting_translated_values.sql

---
+++ exporting_translated_values.sql
@@ -0,0 +1,20 @@
+-- This example showcases how to export translations instead of their base values. 
+-- The query will return list of base names of countries (in english) as well as their polish equivalents.
+-- Use OpenOffice Calc with UTF-8 encoding to avoid formatting problems.
+
+SELECT 
+translations.res_id AS "ID zasobu",
+res_country.name AS "Nazwa bazowa",
+translations.value AS "Nazwa polska"
+FROM 
+(
+SELECT
+res_id,
+value
+FROM ir_translation
+
+WHERE name = 'res.country,name' AND lang = 'pl_PL') AS translations
+
+LEFT JOIN res_country ON res_country.id = translations.res_id
+
+ORDER BY translations.res_id
