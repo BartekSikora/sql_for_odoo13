36599ed85ae2bbb3a41b70e9b1e5900bfebba537
Bartek Sikora <58654131+BartekSikora@users.noreply.github.com>
2022-01-22 16:44:20 +0100

    Update example_product_with_template.sql

--- example_product_with_template.sql
+++ example_product_with_template.sql
@@ -3,7 +3,7 @@
 -- Use OpenOffice Calc with UTF-8 encoding to avoid formatting problems.
 
 SELECT 
-product_template.name AS "Produkt", 
+product_template.name AS "Product", 
 product_product.default_code AS "SKU" 
 FROM product_product
 
